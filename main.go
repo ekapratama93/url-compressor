6ec093f287a4dce865e236cbbb391cc5433c02e9
ekapratama93 <ekapratama93@gmail.com>
2022-05-21 16:59:56 +0700

    fix: validate input for valid URL

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"strings"
 	"time"
 
+	validator "github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
@@ -15,21 +17,22 @@ import (
 )
 
 type Config struct {
-    Port int
-    Host string
+	Port   int    `default:"8000"`
+	Host   string `default:"localhost"`
+	Scheme string `default:"https"`
 }
 
 func main() {
 	var config Config
-    err := envconfig.Process("", &config)
+	err := envconfig.Process("", &config)
 	if err != nil {
-        log.Fatal(err.Error())
-    }
+		log.Fatal(err.Error())
+	}
 
-    app := fiber.New()
+	app := fiber.New()
 
 	app.Use(cache.New(cache.Config{
-		Expiration: 30 * 24 * time.Hour,
+		Expiration:   30 * 24 * time.Hour,
 		CacheControl: true,
 		KeyGenerator: func(c *fiber.Ctx) string {
 			return utils.CopyString(c.OriginalURL())
@@ -37,29 +40,39 @@ func main() {
 	}))
 
 	app.Use(limiter.New(limiter.Config{
-		Max:            5,
-		Expiration:     30 * time.Second,
+		Max:               5,
+		Expiration:        30 * time.Second,
 		LimiterMiddleware: limiter.SlidingWindow{},
 	}))
 
 	app.Use(logger.New(logger.Config{
-		TimeZone:   "Asia/Jakarta",
+		TimeZone: "Asia/Jakarta",
 	}))
 
-	app.Get("/", func (c *fiber.Ctx) error {
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("👋")
 	})
 
-    app.Get("/compress/*", func (c *fiber.Ctx) error {
-		log.Println(c.OriginalURL())
-		url := strings.TrimPrefix(c.OriginalURL(), "/compress/")
-		url = strings.TrimPrefix(url, "/compress?url=")
-		return c.SendString("https://"+config.Host+"/u/"+compress(url))
+	app.Get("/compress/*", func(c *fiber.Ctx) error {
+		requestedUrl := strings.TrimPrefix(c.OriginalURL(), "/compress/")
+		requestedUrl = strings.TrimPrefix(requestedUrl, "/compress?url=")
+
+		isValid := validator.IsURL(requestedUrl)
+		if !isValid {
+			return c.Status(fiber.StatusBadRequest).SendString("Not a valid URL")
+		}
+
+		generatedUrl := url.URL{
+			Scheme: config.Scheme,
+			Host:   config.Host,
+			Path:   compress(requestedUrl),
+		}
+		return c.SendString(generatedUrl.String())
 	})
 
-	app.Get("/u/*", func (c *fiber.Ctx) error {
+	app.Get("/u/*", func(c *fiber.Ctx) error {
 		return c.Redirect(decompress(c.Params("*")), fiber.StatusMovedPermanently)
 	})
 
-    log.Fatal(app.Listen(fmt.Sprintf(":%d", config.Port)))
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", config.Port)))
 }
